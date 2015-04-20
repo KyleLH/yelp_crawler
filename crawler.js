368ad307258d930cb8c11c0a0353b16054734b01
Kyle Holzinger <kylelholzinger@gmail.com>
2015-04-20 16:09:17 -0400

    ic

--- crawler.js
+++ crawler.js
@@ -96,29 +96,9 @@ function save(callback) {
 }
 
 async.series([
-<<<<<<< HEAD
     getYelp,
     getDist,
     getC,
     getM,
     save
-=======
-   function () {
-      yelp.search({term: 'hair', location: 'Boston'}, function (e, data) {
-         console.log(e);
-         console.log(data);
-         res_len = data['businesses'].length
-      });
-   },
-   function () {
-      for ( var i = 20; i <= res_len; i += 20) {
-         console.dir("Recieving at offset " + i);
-         yelp.search({term: 'hair', location: 'Boston', offset: i}, function (e, data) {
-            console.log(e);
-            console.log(data);
-            console.log(data['businesses'].length);
-         });
-      }
-   }
->>>>>>> 3416f42a884f2025d8ee10f77506a2742ff36e7f
 ]);
